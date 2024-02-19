77ff960e735180504af9105c0ec8e4a67e1fa41d
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-02-20 04:04:24 +0530

    first

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 //making of server
 import express from "express";
-
+import path from "fs"
 const app = express ();
 
-app.get('/',(req,res)=>{
-    res.send("hi")
+app.get('/product',(req,res)=>{
+    // res.send("hi")
+    // res.sendStatus(400).send("hi")
+    // res.json({
+    //     sucess: true,
+    //     products : [1,2],
+    // })
+
+     1:09 hr
+
 })
 
 app.listen(5000,() => {
