5c1f0a7d438e559a8b969f205f5a12605b06b4cb
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-02-20 19:21:23 +0530

    add filses

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 //making of server
 import express from "express";
 import path from "path";
+
 const app = express ();
 
-//make public folder static
+//make public folder static /using middleware
 app.use(express.static(path.join(path.resolve(),"public")))
+app.use(express.urlencoded({extended:true}))//can acess data from form
 
 //setting up view engine
 app.set("view engine","ejs")
 
-app.get('/product',(req,res)=>{
-    // res.send("hi")
-    // res.sendStatus(400).send("hi")
-    // res.json({
-    //     sucess: true,
-    //     products : [1,2],
-    // })
-    // const pathLocation = path.resolve();
-    // res.sendFile(path.join(pathLocation,"./index.html"));
-    res.render("index",{name:"huzaifa"}) 
-})
 
 app.get('/',(req,res)=>{
-    res.sendFile("index")
+    res.render("index") 
 })
-
+app.post('/',(req,res)=>{
+    
+})
 
 app.listen(3000,() => {
     console.log("Server is working");
@@ -32,3 +25,48 @@ app.listen(3000,() => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/',(req,res)=>{
+//     // res.send("hi")
+//     // res.sendStatus(400).send("hi")
+//     // res.json({
+//     //     sucess: true,
+//     //     products : [1,2],
+//     // })
+//     // const pathLocation = path.resolve();
+//     // res.sendFile(path.join(pathLocation,"./index.html"));
+//     res.render("index") 
+// })
+
+// app.get('/',(req,res)=>{
+//     res.sendFile("index")
+// })
