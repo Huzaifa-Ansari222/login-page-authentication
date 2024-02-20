a781356617ccc24dc3c0e26da458426351445a94
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-02-20 17:40:15 +0530

    add filses

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 //making of server
 import express from "express";
-import path from "fs"
+import path from "path";
 const app = express ();
+
+//make public folder static
+app.use(express.static(path.join(path.resolve(),"public")))
+
+//setting up view engine
+app.set("view engine","ejs")
 
 app.get('/product',(req,res)=>{
     // res.send("hi")
@@ -10,12 +16,17 @@ app.get('/product',(req,res)=>{
     //     sucess: true,
     //     products : [1,2],
     // })
-
-     1:09 hr
-
+    // const pathLocation = path.resolve();
+    // res.sendFile(path.join(pathLocation,"./index.html"));
+    res.render("index",{name:"huzaifa"}) 
 })
 
-app.listen(5000,() => {
+app.get('/',(req,res)=>{
+    res.sendFile("index")
+})
+
+
+app.listen(3000,() => {
     console.log("Server is working");
 })
 
