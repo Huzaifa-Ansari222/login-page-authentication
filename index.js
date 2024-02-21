6d78bac3056cac63aaeb911f3097d5a66e1ef5f0
Huzaifa-Ansari222 <huzaifaansari656@gmail.com>
2024-02-22 04:40:00 +0530

    add fiels

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import path from "path";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import  Jwt  from "jsonwebtoken";
 import { log } from "console";
 
 //connt to db
@@ -15,6 +16,7 @@ mongoose.connect("mongodb://127.0.0.1:27017",{
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
+    password: String,
 });
 const User = mongoose.model("User",userSchema)//model = collection
 
@@ -30,32 +32,71 @@ app.use(cookieParser())
 app.set("view engine", "ejs")
 
 //handler
-const isAuthenticated = (req, res ,next) => {
+const isAuthenticated = async (req, res ,next) => {
     const token = req.cookies.token; //or can say const {token} = req.cookies
     if (token){ //if login
-        
+        const decoded = Jwt.verify(token,"randomSecrect") //id jwt code
+        // console.log(decoded);
+        req.user = await User.findById(decoded._id) //any auth user can be acess  
         next();  //60 line handler run
     }
     else{ //if logout
-        res.render("login") 
+        res.redirect("/login") 
     }
 }
 
-
-app.get('/',  isAuthenticated, (req,res)=>{ 
-    res.render("logout") //if have token render this
+app.get('/login' ,(req,res) => {
+    res.render("login")
 })
 
-app.post('/login',async (req, res) => {
+app.get('/',  isAuthenticated, (req,res) => { 
+    // console.log(req.user);// user data
+    res.render("logout" ,{name: req.user.name}) //if have token render this
+})
+
+app.post('/login', async (req,res)=>{ 
+    const {email , password} =req.body;
+    let user = await User.findOne({email})
+
+    if(!user) return res.redirect("/register")
+
+    const isMatch = user.password === password;
+
+    if(!isMatch) { //WRONG pass
+        return res.render("login", {message: "Invalid Password!"});
+    }
+    else{
+        const token = Jwt.sign({_id: user._id}, "randomSecrect")
+        res.cookie("token",token ,{ //token exist  and store user id on token
+        httpOnly: true,
+        expires: new Date(Date.now()+ 60 * 1000)
+        })
+        res.redirect("/")
+    }
+
+})
+
+app.get('/register', (req,res)=>{ 
+    res.render("register") //
+})
+
+app.post('/register',async (req, res) => {
     // console.log(req.body);
-    const {name, email} = req.body
+    const {name, email, password} = req.body
+
+    let user = await User.findOne({ email })
+    if(user){
+        return res.redirect("/login");
+    }
     // get data here 
-    const user =  await User.create({ //we get user id here
+    user =  await User.create({ //we get user id here
         name,
         email,
+        password,
     })
-    //inspect>application>cookies
-    res.cookie("token",user._id ,{ //token exist  and store user id on token
+    const token = Jwt.sign({_id: user._id}, "randomSecrect")
+    // console.log(token);
+    res.cookie("token",token ,{ //token exist  and store user id on token
     httpOnly: true,
     expires: new Date(Date.now()+ 60 * 1000)
     })
@@ -63,7 +104,7 @@ app.post('/login',async (req, res) => {
 })
 
 app.get('/logout',(req, res) => {
-    res.cookie("token",null ,{
+    res.cookie("token",null ,{    //inspect>application>cookies
     httpOnly: true,
     expires: new Date(Date.now())
     })
